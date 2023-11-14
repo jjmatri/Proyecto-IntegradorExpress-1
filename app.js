de3489f80f44b1b67db45fc0d204b2a47d208b2b
Jaime Salinas <jaime.salinas@supergirosatlantico.co>
2023-11-14 16:48:48 -0500

    project-4 cambios

--- app.js
+++ app.js
@@ -1,94 +1,46 @@
-const express = require('express');
-const { midd} = require('./middlewares')
-
-
-const booksRouter = require('./list-view-router')
-const ordersRouter = require('./list-edit-router')
-
-require('dotenv').config();
-
-
-const jwt = require('jsonwebtoken');
-
-
-const PORT = process.env.PORT;
-const LLAVE_SECRETA = process.env.LLAVE_SECRETA;
-
-
-
+const express = require("express");
+// const { midd } = require("./middlewares");
+// const booksRouter = require("./list-view-router");
+// const ordersRouter = require("./list-edit-router");
+// require("dotenv").config();
+const jwt = require("jsonwebtoken");
+// const PORT = 3000;
+// const LLAVE_SECRETA = 'secret';
 const app = express();
 const port = 1000;
-
-app.use(express.json())
-
-
-app.use('/completed-tasks', booksRouter)
-app.use('/tasks', ordersRouter)
-app.use(midd)
-			
+app.use(express.json());
+// app.use("/completed-tasks", booksRouter);
+// app.use("/tasks", ordersRouter);
+// app.use(midd);
 //DataBase Simulation
-const userDb = [{ userName: 'user123',password: 'elMasGuapoDelMundo', rol: "admin"},
-                {userName: 'user456',password: 'jaja', rol: "user"
-}]
-
+const userDb = [
+  { userName: "user123", password: "elMasGuapoDelMundo", rol: "admin" },
+  { userName: "user456", password: "jaja", rol: "user" },
+];
 function authMiddleware(req, res, next) {
-    const token = req.headers.authorization;
+  const token = req.headers.authorization;
+  if (!token) {
+    res.status(401).send("Debes porporcionar un token");
+  }
+  jwt.verify(token, "secret key", (err,decoded) => {
+    console.log(decoded)
+    req.rol=decoded.rol;
+    if(err){
 
-    /**
-     * 1. Token exista
-     * 2. Token sea valido
-     */
-
-    if(!token) {
-        res.status(401).send('Debes porporcionar un token')
+        console.log(err)
     }
-
-   /* try {
-        const tokenDecrypted = jwt.verify(token, LLAVE_SECRETA)
-        req.data = tokenDecrypted;
-        next();
-    } catch (error) {
-        res.status(401).send('No autorizado: debes proveer un token valido')
-    }
-     */
-         console.log(token);
-         console.log(LLAVE_SECRETA);
-            // Verificar el token
-    jwt.verify(token, 'secret key', (err, decoded) => {
-        /*if (err) {
-            return res.status(403).json({ error: 'Token verification failed' });
-        }*/
-
-        // Verificar el rol en el token decodificado
-        const { rol } = decoded;
-        if (rol == "admin" || rol == "user") {
-            req.rol = rol; // Añadir el rol al request
-            next();
-        } else {
-
-            return res.status(403).json({ error: 'Access not allowed' });
-           //res.json({ error });
-        }
-    });
-
-
-
+    next();
+  });
 }
-
-app.post('/login', (req, res) => {
-    
-    const user = req.body.user;
-    const pass = req.body.password;
-
-    
-    /*if(user === userDb.userName && pass === userDb.password){
+app.post("/login", (req, res) => {
+  const user = req.body.user;
+  const pass = req.body.password;
+  /*if(user === userDb.userName && pass === userDb.password){
         // I can create the JWT
         const payload = {
             rol: userDb.rol
         }
-
         const token = jwt.sign(payload, LLAVE_SECRETA,{algorithm: "HS256"})
-
         res.status(200).send({
             message: 'Bienvenido',
             token
@@ -96,62 +48,47 @@ app.post('/login', (req, res) => {
     } else {
         res.status(401).send('Contraseña o Usuario incorrecto')
     }*/
-
-    const index = userDb.findIndex(userDb => userDb.password === pass, userDb.user === user);
-    if (index === -1){
-        res.status(401).send({error:"Invalid user name or password"});
-      }else{
-        const payload =userDb[index];
-    
-        const token = jwt.sign(payload,LLAVE_SECRETA,{algorithm: "HS256"});
-    
-        res.json({ token });
-        //res.status(200).header("Authorization",token).json({token});
-  
-      }
-  
-
-})
-app.get('/books', authMiddleware, (req, res) => {
-    //res.status(200).send('Esta es una lista de libros')
-
-
-    if (req.role == "user" || req.role == "admin") {
-
-        res.json({ message: "Acceso concedido" });
-    
-      } else {
-    
-        res.status(200).json({ error: "Access not allowed" });
-    
-      }
-
-})
-
-
-app.get('/', (req, res) => {
-    res.send('Hola mundo')
-    //res.send('Hola mundo')
-
-    const tareas =[ {
-        "id":"123456",
-        "isCompleted":false,
-         "description":"Walk the dog"
+  const index = userDb.findIndex(
+    (userDb) => userDb.password === pass,
+    userDb.user === user
+  );
+  if (index === -1) {
+    res.status(401).send({ error: "Invalid user name or password" });
+  } else {
+    const payload = userDb[index];
+    const token = jwt.sign(payload, "secret key", { expiresIn: "5m" });
+    res.json({ token });
+    //res.status(200).header("Authorization",token).json({token});
+  }
+});
+app.get("/books", authMiddleware, (req, res) => {
+  //res.status(200).send('Esta es una lista de libros')
+ console.log( {rol: req.rol})
+  if (req.rol == "user" || req.rol == "admin") {
+    res.json({ message: "Acceso concedido" });
+  } else {
+    res.status(401).json({ error: "Access not allow" });
+  }
+});
+app.get("/", (req, res) => {
+  res.send("Hola mundo");
+  //res.send('Hola mundo')
+  const tareas = [
+    {
+      id: "123456",
+      isCompleted: false,
+      description: "Walk the dog",
     },
     {
-        "id":"1234",
-        "isCompleted":true,
-        "description":"Walk the cat"
-        
-    }]
-    //const objeto = JSON.stringify(datos);
-    //console.log(objeto.id);
-    console.log(JSON.stringify(tareas));
-    
-    
-}) 
-
-
+      id: "1234",
+      isCompleted: true,
+      description: "Walk the cat",
+    },
+  ];
+  //const objeto = JSON.stringify(datos);
+  //console.log(objeto.id);
+  console.log(JSON.stringify(tareas));
+});
 app.listen(port, () => {
-    console.log(`Example ap listening on port ${port}`)
-})
+  console.log(`Example ap listening on port ${port}`);
+});
