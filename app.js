91d01a245df2c5ab8e0218fed6e0536225b22989
Jaime Salinas <jaime.salinas@supergirosatlantico.co>
2023-11-23 17:17:35 -0500

    actualizando app

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const express = require("express");
  const jwt = require("jsonwebtoken");
 
 const app = express();
-const LLAVE_SECRETA = 'secret';
+const LLAVE_SECRETA = process.env.LLAVE_SECRETA;
 const port = 1000;
 app.use(express.json());
  app.use("/completed-tasks", booksRouter);
